875d6a54fbe02e42465dc5447236c3d688e74592
tushagg <tusharagrawal058@gmail.com>
2025-09-24 22:40:23 +0530

    modified env url and codes

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api",
+ baseURL : import.meta.env.MODE === "development" ? import.meta.env.VITE_BASE_URL + "/api" : import.meta.env.VITE_SOCKET_URL+"/api",
+
   withCredentials: true,
 });
