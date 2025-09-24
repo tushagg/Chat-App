ba1bfa3e4d522ae13a3886b49324ca50aa9319fe
tushagg <tusharagrawal058@gmail.com>
2025-09-24 22:58:00 +0530

    fixed cors issue

--- backend/src/index.js
+++ backend/src/index.js
@@ -18,9 +18,15 @@ const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(
+/*app.use(
   cors({
     origin: "http://localhost:5173",
+    credentials: true,
+  })
+);*/
+app.use(
+  cors({
+    origin: [process.env.CLIENT_URL , "http://localhost:5173"],
     credentials: true,
   })
 );
@@ -28,13 +34,13 @@ app.use(
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+//if (process.env.NODE_ENV === "production") {
+ // app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-  });
-}
+ // app.get("*", (req, res) => {
+ //   res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+ // });
+//}
 
 server.listen(PORT, () => {
   console.log("server is running on PORT:" + PORT);
